18a81298f45aad8ca2432bf46b3abd52709db5cc
chrisIyer <hellohotbox420@gmail.com>
2025-08-22 12:45:28 -0400

    routes all tested and working.

--- server/server.js
+++ server/server.js
@@ -17,5 +17,5 @@ mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/rough-auto'
 // Routes
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
